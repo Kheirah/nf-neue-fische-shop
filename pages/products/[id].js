31e82d3cc651c5ac2a39e06ca2f6ab0e0665b7e4
Ryan Ceres <c.nizz@web.de>
2022-10-12 15:37:23 +0200

    add srr session
    - add data
    - add services
    - add serversideprops to index of products
    - add serversideprops to dynamic page of products

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,7 +1,59 @@
-import { useRouter } from "next/router";
+import { getProductById } from "../../services/productServices";
+import Head from "next/head";
+import Link from "next/link";
+import styled from "styled-components";
 
-export default function Product() {
-  const router = useRouter();
-  const { id } = router.query;
-  return <h1>Produkt {id}</h1>;
+export async function getServerSideProps(ctx) {
+  const { id } = ctx.params;
+  const product = await getProductById(id);
+
+  return {
+    props: product,
+  };
 }
+
+export default function Product({ name, description, price, category }) {
+  return (
+    <>
+      <Head>
+        <title>Product: {name}</title>
+      </Head>
+      <h1>Produkt {name}</h1>
+      <p>Preis: {price}</p>
+      <h2>Details</h2>
+      <Table>
+        <tbody>
+          <tr>
+            <TdBold>Beschreibung:</TdBold>
+            <td>{description}</td>
+          </tr>
+          <tr>
+            <TdBold>Kategorie:</TdBold>
+            <td>{category}</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Link href={`/products`}>Alle Produkte</Link>
+    </>
+  );
+}
+
+const Price = styled.p`
+  font-size: 1.5rem;
+`;
+
+const Table = styled.table`
+  font-size: 0.9rem;
+  text-align: left;
+  text-align: left;
+  max-width: 40rem;
+  margin-bottom: 2rem;
+`;
+
+const TdBold = styled.td`
+  vertical-align: baseline;
+  font-weight: bold;
+  display: block;
+  margin-right: 2rem;
+  padding-left: 0;
+`;
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,18 +1,30 @@
 import GridList from "../../components/GridList";
 import Link from "next/link";
+import Head from "next/head";
+import { getAllProducts } from "../../services/productServices";
 
-export default function Products() {
+export async function getServerSideProps() {
+  const products = await getAllProducts();
+
+  return {
+    props: { products },
+  };
+}
+
+export default function Products({ products }) {
   return (
     <>
+      <Head>
+        <title>Alle Produkte</title>
+      </Head>
       <h1>Products</h1>
       <p>Liste alle Produkte</p>
       <GridList>
-        <li>
-          <Link href="/products/guppy">Guppy</Link>
-        </li>
-        <li>
-          <Link href="/products/Regenbogenfisch">Regenbogenfisch</Link>
-        </li>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
+          </li>
+        ))}
       </GridList>
     </>
   );
